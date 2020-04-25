f4f0474025b99b7c8aa2c0920b7dc73c9553963b
Kolon <sea7151_@naver.com>
2020-04-25 03:27:13 +0000

    React Learn Day 1

---
+++ sjy/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InputBox from "./components/InputBox";
+import PhoneList from "./components/PhoneList";
+import './App.css';
+
+function App() {
+  return (
+  <div className="container">
+		<InputBox />	
+		<PhoneList />
+	</div>
+  );
+}
+
+export default App;
